083075676541ad84344e5096dcf1b3504bf123a1
Adrián de la Parra Hernández <52699504+adriandlph@users.noreply.github.com>
2026-06-27 09:45:17 -0700

    Baseline

---
+++ src/main/java/com/adlph/transport/automotive/kitt/server/KittServerApplication.java
@@ -0,0 +1,13 @@
+package com.adlph.transport.automotive.kitt.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KittServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adlph/transport/automotive/kitt/server/KittServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adlph.transport.automotive.kitt.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KittServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
