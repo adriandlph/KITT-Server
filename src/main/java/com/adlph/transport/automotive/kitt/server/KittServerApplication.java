7b1d62f52af6b05fe945c549e74ef92641970424
Adrián de la Parra Hernández <52699504+adriandlph@users.noreply.github.com>
2026-06-28 09:36:32 -0700

    Update POM and add and configure log4j
    - Remove testing
    - Add log4j
    - Configure log4j

--- src/main/java/com/adlph/transport/automotive/kitt/server/KittServerApplication.java
+++ src/main/java/com/adlph/transport/automotive/kitt/server/KittServerApplication.java
@@ -2,12 +2,16 @@ package com.adlph.transport.automotive.kitt.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.log4j.Logger;
 
 @SpringBootApplication
 public class KittServerApplication {
+	private static final Logger LOGGER = Logger.getLogger(KittServerApplication.class.getName());
 
 	public static void main(String[] args) {
+		LOGGER.info("Starting KITT server...");
 		SpringApplication.run(KittServerApplication.class, args);
+		LOGGER.info("KITT server started!");
 	}
 
 }
